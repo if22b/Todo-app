43b3fa16e41befb50075e34790fada557e10a944
if22b133 <if22b133@technikum-wien.at>
2024-05-27 16:40:14 +0200

    Integrate posthog

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
 import './assets/main.css'
+import posthogPlugin from "./plugins/posthog"; //import the plugin. 
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(posthogPlugin); //install the plugin
+app.mount('#app')
---
+++ src/plugins/posthog.js
@@ -0,0 +1,12 @@
+import posthog from "posthog-js";
+
+export default {
+  install(app) {
+    app.config.globalProperties.$posthog = posthog.init(
+      'phc_mUTrNZYbj4qQyOGn2K1szr3qokj67CktsCfM60ZbBVt',
+      {
+        api_host: 'https://us.i.posthog.com',
+      }
+    );
+  },
+};
