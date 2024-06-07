5031d730cd7b992641094cbfeff7b1e24eba2468
if22b133 <if22b133@technikum-wien.at>
2024-06-07 17:09:12 +0200

    fix posthog

--- src/plugins/posthog.js
+++ src/plugins/posthog.js
@@ -2,11 +2,10 @@ import posthog from 'posthog-js';
 
 export default {
   install(app) {
-    app.config.globalProperties.$posthog = posthog.init(
-      'phc_mUTrNZYbj4qQyOGn2K1szr3qokj67CktsCfM60ZbBVt',
-      {
-        api_host: 'https://us.i.posthog.com',
-      }
-    );
+    posthog.init('phc_mUTrNZYbj4qQyOGn2K1szr3qokj67CktsCfM60ZbBVt', {
+      api_host: 'https://us.i.posthog.com',
+    });
+
+    app.config.globalProperties.$posthog = posthog;
   },
 };
