bc9b9ca153e103de6d46ee02346345d63fbdfe14
if22b133 <if22b133@technikum-wien.at>
2024-06-07 16:52:44 +0200

    try fixing posthog

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+      'posthog-js': path.resolve(__dirname, './plugins/posthog.js'),
     },
   },
   build: {
