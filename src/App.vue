0f3a4693607ff88b0037d08aac5e7b24ec471418
if22b133 <if22b133@technikum-wien.at>
2024-06-10 17:00:18 +0200

    without button

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,8 @@ import { RouterView } from 'vue-router';
 <template>
   <div id="app">
     <RouterView />
-    <RegistrationButton />
   </div>
 </template>
-
-<script>
-import RegistrationButton from './components/RegistrationButton.vue';
-
-export default {
-  components: {
-    RegistrationButton
-  }
-};
-</script>
 
 <style scoped>
 /* Add any additional styles here */
